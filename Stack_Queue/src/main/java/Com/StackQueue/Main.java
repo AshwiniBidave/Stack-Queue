4afcb95f87b5df69ee6b8157b5b0a666ea703cfb
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-01 14:37:52 +0530

    OPERATION:perform Peek and pop

--- Stack_Queue/src/main/java/Com/StackQueue/Main.java
+++ Stack_Queue/src/main/java/Com/StackQueue/Main.java
@@ -11,6 +11,11 @@ public class Main {
 		stack.push(30);
 		stack.push(56);
 	    stack.Display();
+	    stack.peek();
+        stack.pop();
+        stack.Display();
+        stack.isEmpty();
+
 		
 
 
--- Stack_Queue/src/main/java/Com/StackQueue/StackOperation.java
+++ Stack_Queue/src/main/java/Com/StackQueue/StackOperation.java
@@ -25,6 +25,33 @@ public class StackOperation {
             temp = temp.next;
         }
     }
+	public void peek()
+    {
+        if (top == null)
+        {
+        	 System.out.println("Stack is Empty...");
+        }
+        System.out.println("\nTop Most Element in Stack : " + top.data);
+    }
+
+    public void pop()
+    {
+        if (top == null)
+        {
+        	 System.out.println("Stack is Empty... ");
+        }
+        System.out.println("Deleted Element : " + top.data);
+        top = top.next;
+    }
+    public void isEmpty()
+    {
+        while (top != null)
+        {
+            peek();
+            pop();
+        }
+        System.out.println("Stack is Empty...");
+    }
 	
 
 }
